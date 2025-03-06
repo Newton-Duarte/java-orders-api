ae37084538fae3a3f258c10e590f1819908a95bf
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-06 18:09:30 -0300

    feat: add user entity class

---
+++ src/main/java/com/newtonduarte/orders_api/domain/UserEntity.java
@@ -0,0 +1,21 @@
+package com.newtonduarte.orders_api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    private Long id;
+    private String name;
+    private String email;
+}
