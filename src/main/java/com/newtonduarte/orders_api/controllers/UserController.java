d0f8a413e8473d85f5c6295c975d7b83b2f3673d
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-06 21:02:51 -0300

    feat: add user controller create, update and partial update methods

--- src/main/java/com/newtonduarte/orders_api/controllers/UserController.java
+++ src/main/java/com/newtonduarte/orders_api/controllers/UserController.java
@@ -1,17 +1,73 @@
 package com.newtonduarte.orders_api.controllers;
 
-import com.newtonduarte.orders_api.domain.UserEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.newtonduarte.orders_api.domain.dto.UserDto;
+import com.newtonduarte.orders_api.domain.entities.UserEntity;
+import com.newtonduarte.orders_api.mappers.Mapper;
+import com.newtonduarte.orders_api.services.UserService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
-    @GetMapping(path = "users")
-    public UserEntity getUsers() {
-        return UserEntity.builder()
-                .id(1L)
-                .name("Newton Duarte")
-                .email("newton@email.com")
-                .build();
+    private final UserService userService;
+    private final Mapper<UserEntity, UserDto> userMapper;
+
+    public UserController(final UserService userService, final Mapper<UserEntity, UserDto> userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping(path = "/users")
+    public List<UserDto> getUsers() {
+        List<UserEntity> users = userService.findAll();
+        return users.stream().map(userMapper::mapTo).collect(Collectors.toList());
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable("id") Long id) {
+        Optional<UserEntity> foundUser = userService.findOne(id);
+
+        return foundUser.map(userEntity -> {
+            UserDto userDto = userMapper.mapTo(userEntity);
+            return new ResponseEntity<>(userDto, HttpStatus.OK);
+        }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserEntity userEntity = userMapper.mapFrom(userDto);
+        UserEntity savedUserEntity = userService.save(userEntity);
+
+        return new ResponseEntity<>(userMapper.mapTo(savedUserEntity), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/users/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long id, @RequestBody UserDto userDto) {
+        if (!userService.isExists(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        UserEntity userEntity = userMapper.mapFrom(userDto);
+        UserEntity savedUserEntity = userService.save(userEntity);
+
+        return new ResponseEntity<>(userMapper.mapTo(savedUserEntity), HttpStatus.OK);
+    }
+
+    @PatchMapping(path = "/users/{id}")
+    public ResponseEntity<UserDto> partialUpdateUser(@PathVariable("id") Long id, @RequestBody UserDto userDto) {
+        if (!userService.isExists(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        UserEntity userEntity = userMapper.mapFrom(userDto);
+        UserEntity updatedUserEntity = userService.partialUpdate(id, userEntity);
+
+        return new ResponseEntity<>(userMapper.mapTo(updatedUserEntity), HttpStatus.OK);
     }
 }
