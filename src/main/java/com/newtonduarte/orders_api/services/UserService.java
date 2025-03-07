0f7c109090502d2c24f178604c12b5dd32633bec
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-06 21:02:18 -0300

    feat: add user service interface

---
+++ src/main/java/com/newtonduarte/orders_api/services/UserService.java
@@ -0,0 +1,14 @@
+package com.newtonduarte.orders_api.services;
+
+import com.newtonduarte.orders_api.domain.entities.UserEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<UserEntity> findAll();
+    UserEntity save(UserEntity userEntity);
+    Optional<UserEntity> findOne(Long id);
+    boolean isExists(Long id);
+    UserEntity partialUpdate(Long id, UserEntity userEntity);
+}
