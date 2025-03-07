0eb71a66f4f4da765e1ee5de91af05eba0db4c56
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-06 21:03:24 -0300

    fix: test data utils object builder

--- src/test/java/com/newtonduarte/orders_api/TestDataUtils.java
+++ src/test/java/com/newtonduarte/orders_api/TestDataUtils.java
@@ -1,13 +1,12 @@
 package com.newtonduarte.orders_api;
 
-import com.newtonduarte.orders_api.domain.UserEntity;
+import com.newtonduarte.orders_api.domain.entities.UserEntity;
 
 public final class TestDataUtils {
     private TestDataUtils() {}
 
     public static UserEntity createTestUserEntityA() {
         return UserEntity.builder()
-                .id(1L)
                 .name("John Doe")
                 .email("johndoe@email.com")
                 .build();
@@ -15,7 +14,6 @@ public final class TestDataUtils {
 
     public static UserEntity createTestUserEntityB() {
         return UserEntity.builder()
-                .id(2L)
                 .name("Jane Doe")
                 .email("janedoe@email.com")
                 .build();
@@ -23,7 +21,6 @@ public final class TestDataUtils {
 
     public static UserEntity createTestUserEntityC() {
         return UserEntity.builder()
-                .id(1L)
                 .name("John Smith")
                 .email("johnsmith@email.com")
                 .build();
