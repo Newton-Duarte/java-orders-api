bcb641ee60bb331f31fc2e234dcc424fd820c6a2
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-06 21:03:03 -0300

    feat: add user service implementation

---
+++ src/main/java/com/newtonduarte/orders_api/services/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.newtonduarte.orders_api.services.impl;
+
+import com.newtonduarte.orders_api.domain.entities.UserEntity;
+import com.newtonduarte.orders_api.repositories.UserRepository;
+import com.newtonduarte.orders_api.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserEntity> findAll() {
+        return StreamSupport.stream(userRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserEntity save(UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    @Override
+    public Optional<UserEntity> findOne(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public boolean isExists(Long id) {
+        return userRepository.existsById(id);
+    }
+
+    @Override
+    public UserEntity partialUpdate(Long id, UserEntity userEntity) {
+        userEntity.setId(id);
+
+        return userRepository.findById(id).map(existingAuthor -> {
+            Optional.ofNullable(userEntity.getName()).ifPresent(existingAuthor::setName);
+            Optional.ofNullable(userEntity.getEmail()).ifPresent(existingAuthor::setEmail);
+            return userRepository.save(existingAuthor);
+        }).orElseThrow(() -> new RuntimeException("User does not exist"));
+    }
+}
