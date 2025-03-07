856b8a3a509561768c8e28f3bde39277c077cd0c
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-07 11:50:34 -0300

    feat: add validation annotations to user dto

--- src/main/java/com/newtonduarte/orders_api/domain/dto/UserDto.java
+++ src/main/java/com/newtonduarte/orders_api/domain/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.newtonduarte.orders_api.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserDto {
     private Long id;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotBlank(message = "Email is mandatory")
     private String email;
 }
