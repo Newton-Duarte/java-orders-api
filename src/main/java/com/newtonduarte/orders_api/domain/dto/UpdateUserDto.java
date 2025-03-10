983388979d067469b593087399ef786854f6967c
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 20:39:00 -0300

    feat: add update user dto

---
+++ src/main/java/com/newtonduarte/orders_api/domain/dto/UpdateUserDto.java
@@ -0,0 +1,25 @@
+package com.newtonduarte.orders_api.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserDto {
+    @NotNull(message = "User ID is required")
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "User name must be between {min} and {max} characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    private String email;
+}
