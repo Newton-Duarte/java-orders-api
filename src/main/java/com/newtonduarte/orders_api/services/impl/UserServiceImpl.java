dbbb42c329c2cbef62b9494ceeb075c6f4078169
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 20:45:28 -0300

    feat: implement create and update user

--- src/main/java/com/newtonduarte/orders_api/services/impl/UserServiceImpl.java
+++ src/main/java/com/newtonduarte/orders_api/services/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.newtonduarte.orders_api.services.impl;
 
+import com.newtonduarte.orders_api.domain.dto.CreateUserDto;
+import com.newtonduarte.orders_api.domain.dto.UpdateUserDto;
 import com.newtonduarte.orders_api.domain.entities.UserEntity;
 import com.newtonduarte.orders_api.repositories.UserRepository;
 import com.newtonduarte.orders_api.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -24,8 +28,42 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserEntity save(UserEntity userEntity) {
-        return userRepository.save(userEntity);
+    public UserEntity createUser(CreateUserDto createUserDto) {
+        Optional<UserEntity> existingUser = userRepository
+                .findByEmail(createUserDto.getEmail());
+
+        if (existingUser.isPresent()) {
+            throw new IllegalStateException("User with same email already exists");
+        }
+
+        UserEntity newUser = new UserEntity();
+        newUser.setName(createUserDto.getName());
+        newUser.setEmail(createUserDto.getEmail());
+
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    public UserEntity updateUser(Long id, UpdateUserDto updateUserDto) {
+        UserEntity existingUser = userRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id" + id));
+
+        Optional<UserEntity> existingUserByEmail = userRepository
+                .findByEmail(updateUserDto.getEmail());
+
+        if (existingUserByEmail.isPresent()) {
+            if (!Objects.equals(existingUserByEmail.get().getId(), updateUserDto.getId())) {
+                throw new IllegalStateException("User with same email already exists");
+            }
+        }
+
+        UserEntity newUser = new UserEntity();
+        newUser.setId(updateUserDto.getId());
+        newUser.setName(updateUserDto.getName());
+        newUser.setEmail(updateUserDto.getEmail());
+
+        return userRepository.save(newUser);
     }
 
     @Override
@@ -36,17 +74,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean isExists(Long id) {
         return userRepository.existsById(id);
-    }
-
-    @Override
-    public UserEntity partialUpdate(Long id, UserEntity userEntity) {
-        userEntity.setId(id);
-
-        return userRepository.findById(id).map(existingAuthor -> {
-            Optional.ofNullable(userEntity.getName()).ifPresent(existingAuthor::setName);
-            Optional.ofNullable(userEntity.getEmail()).ifPresent(existingAuthor::setEmail);
-            return userRepository.save(existingAuthor);
-        }).orElseThrow(() -> new RuntimeException("User does not exist"));
     }
 
     @Override
