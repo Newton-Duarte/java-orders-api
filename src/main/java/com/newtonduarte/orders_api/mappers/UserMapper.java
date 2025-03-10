fb54daeb1b3d3c10734b1117b8bdba9beab68f71
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 19:48:17 -0300

    refactor: user mapper

---
+++ src/main/java/com/newtonduarte/orders_api/mappers/UserMapper.java
@@ -0,0 +1,12 @@
+package com.newtonduarte.orders_api.mappers;
+
+import com.newtonduarte.orders_api.domain.dto.UserDto;
+import com.newtonduarte.orders_api.domain.entities.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    UserDto toDto(UserEntity user);
+    UserEntity toEntity(UserDto userDto);
+}
