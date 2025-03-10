670a90fdcdb678a5abcce4a4d35d28fd009d9d4a
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 19:48:06 -0300

    refactor: product mapper

---
+++ src/main/java/com/newtonduarte/orders_api/mappers/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.newtonduarte.orders_api.mappers;
+
+import com.newtonduarte.orders_api.domain.dto.ProductDto;
+import com.newtonduarte.orders_api.domain.entities.ProductEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ProductMapper {
+    ProductDto toDto(ProductEntity product);
+    ProductEntity toEntity(ProductDto productDto);
+}
