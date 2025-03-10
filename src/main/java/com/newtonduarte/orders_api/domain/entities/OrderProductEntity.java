46ac77ce119e4109c9b015802467e58d5bfa8fdf
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 20:41:06 -0300

    feat: implement equals and hash code

--- src/main/java/com/newtonduarte/orders_api/domain/entities/OrderProductEntity.java
+++ src/main/java/com/newtonduarte/orders_api/domain/entities/OrderProductEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,9 +26,24 @@ public class OrderProductEntity {
     @JoinColumn(name = "product_id")
     private ProductEntity product;
 
+    @Column(nullable = false)
     private Integer quantity;
 
+    @Column(nullable = false)
     private BigDecimal price;
 
+    @Column(nullable = false)
     private BigDecimal total;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderProductEntity that = (OrderProductEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(order, that.order) && Objects.equals(product, that.product) && Objects.equals(quantity, that.quantity) && Objects.equals(price, that.price) && Objects.equals(total, that.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order, product, quantity, price, total);
+    }
 }
