18ec90aa91a0ef5fa45669829a2f80f10c018810
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 11:34:15 -0300

    feat: add order product entity

---
+++ src/main/java/com/newtonduarte/orders_api/domain/entities/OrderProductEntity.java
@@ -0,0 +1,33 @@
+package com.newtonduarte.orders_api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "order_products")
+public class OrderProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id", nullable = false)
+    private OrderEntity order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    private BigDecimal total;
+}
