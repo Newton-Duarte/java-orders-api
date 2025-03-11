0e981f9b0397de87e4ce2fd0768807dfab4164a0
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-10 21:09:13 -0300

    test: add more test date utils dto creation

--- src/test/java/com/newtonduarte/orders_api/TestDataUtils.java
+++ src/test/java/com/newtonduarte/orders_api/TestDataUtils.java
@@ -1,7 +1,13 @@
 package com.newtonduarte.orders_api;
 
+import com.newtonduarte.orders_api.domain.dto.CreateProductDto;
+import com.newtonduarte.orders_api.domain.dto.CreateUserDto;
+import com.newtonduarte.orders_api.domain.dto.UpdateProductDto;
+import com.newtonduarte.orders_api.domain.dto.UpdateUserDto;
 import com.newtonduarte.orders_api.domain.entities.ProductEntity;
 import com.newtonduarte.orders_api.domain.entities.UserEntity;
+
+import java.math.BigDecimal;
 
 public final class TestDataUtils {
     private TestDataUtils() {}
@@ -15,7 +21,7 @@ public final class TestDataUtils {
 
     public static UserEntity createTestUserEntityB() {
         return UserEntity.builder()
-                .name("Jane Doe")
+                .name("Jane doe")
                 .email("janedoe@email.com")
                 .build();
     }
@@ -27,24 +33,40 @@ public final class TestDataUtils {
                 .build();
     }
 
+    public static CreateUserDto createTestCreateUserDtoA() {
+        return CreateUserDto.builder()
+                .name("John Doe")
+                .email("johndoe@email.com")
+                .build();
+    }
+
+    public static UpdateUserDto createTestUpdateUserDtoA() {
+        return UpdateUserDto.builder()
+                .id(1L)
+                .name("John Doe")
+                .email("johndoe@email.com")
+                .build();
+    }
+
     public static ProductEntity createTestProductEntityA() {
         return ProductEntity.builder()
                 .name("Product A")
-                .price(10.0)
+                .price(BigDecimal.valueOf(10.00))
                 .build();
     }
 
-    public static ProductEntity createTestProductEntityB() {
-        return ProductEntity.builder()
-                .name("Product B")
-                .price(15.0)
+    public static CreateProductDto createTestCreateProductDtoA() {
+        return CreateProductDto.builder()
+                .name("Product A")
+                .price(BigDecimal.valueOf(10.00))
                 .build();
     }
 
-    public static ProductEntity createTestProductEntityC() {
-        return ProductEntity.builder()
-                .name("Product C")
-                .price(20.0)
+    public static UpdateProductDto createTestUpdateProductDtoA() {
+        return UpdateProductDto.builder()
+                .id(1L)
+                .name("Product A")
+                .price(BigDecimal.valueOf(10.00))
                 .build();
     }
 }
