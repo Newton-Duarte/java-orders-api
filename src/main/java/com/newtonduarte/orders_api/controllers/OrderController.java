9b96a6585f857787b77b381d50bd657b49e80676
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-19 18:05:08 -0300

    fix: create and update order validation

--- src/main/java/com/newtonduarte/orders_api/controllers/OrderController.java
+++ src/main/java/com/newtonduarte/orders_api/controllers/OrderController.java
@@ -7,6 +7,7 @@ import com.newtonduarte.orders_api.domain.dto.UpdateOrderDto;
 import com.newtonduarte.orders_api.domain.entities.OrderEntity;
 import com.newtonduarte.orders_api.mappers.OrderMapper;
 import com.newtonduarte.orders_api.services.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,13 +36,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderDto> createOrder(@RequestBody CreateOrderDto createOrderDto) {
+    public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody CreateOrderDto createOrderDto) {
         OrderEntity orderEntity = orderService.createOrder(orderMapper.toCreateOrderRequest(createOrderDto));
         return new ResponseEntity<>(orderMapper.toDto(orderEntity), HttpStatus.CREATED);
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<OrderDto> updateOrder(@PathVariable Long id, @RequestBody UpdateOrderDto updateOrderDto) {
+    public ResponseEntity<OrderDto> updateOrder(@PathVariable Long id, @Valid @RequestBody UpdateOrderDto updateOrderDto) {
         OrderEntity updateOrder = orderService.updateOrder(id, orderMapper.toUpdateOrderRequest(updateOrderDto));
         return ResponseEntity.ok(orderMapper.toDto(updateOrder));
     }
