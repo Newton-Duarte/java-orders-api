0eaa0e82a4db05ace052df92025e71f7422c4c36
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-23 11:32:32 -0300

    chore: add description to user controller swagger

--- src/main/java/com/newtonduarte/orders_api/controllers/UserController.java
+++ src/main/java/com/newtonduarte/orders_api/controllers/UserController.java
@@ -7,7 +7,10 @@ import com.newtonduarte.orders_api.domain.dto.UserDto;
 import com.newtonduarte.orders_api.domain.entities.UserEntity;
 import com.newtonduarte.orders_api.mappers.UserMapper;
 import com.newtonduarte.orders_api.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,11 +24,16 @@ import java.util.Optional;
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
 @SecurityRequirement(name = SecurityConfig.SECURITY)
+@Tag(name = "Users", description = "Endpoints for User entity (requires auth)")
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
     @GetMapping
+    @Operation(summary = "Get a list of users")
+    @ApiResponse(responseCode = "200", description = "Request success")
+    @ApiResponse(responseCode = "403", description = "Forbidden request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<List<UserDto>> getUsers() {
         List<UserEntity> users = userService.findAll();
         List<UserDto> usersDto = users.stream().map(userMapper::toDto).toList();
@@ -33,6 +41,11 @@ public class UserController {
     }
 
     @GetMapping(path = "/{id}")
+    @Operation(summary = "Get a single user passing by ID")
+    @ApiResponse(responseCode = "200", description = "Request success")
+    @ApiResponse(responseCode = "403", description = "Forbidden request")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         Optional<UserEntity> foundUser = userService.findOne(id);
 
@@ -43,6 +56,12 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a single user passing the required fields")
+    @ApiResponse(responseCode = "201", description = "User created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid request body")
+    @ApiResponse(responseCode = "403", description = "Forbidden request")
+    @ApiResponse(responseCode = "409", description = "Email already exist")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody CreateUserDto createUserDto) {
         UserEntity createdUserEntity = userService.createUser(createUserDto);
 
@@ -50,6 +69,12 @@ public class UserController {
     }
 
     @PutMapping(path = "/{id}")
+    @Operation(summary = "Update a single user passing the user id and the required fields")
+    @ApiResponse(responseCode = "200", description = "User updated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid request body")
+    @ApiResponse(responseCode = "403", description = "Forbidden request")
+    @ApiResponse(responseCode = "409", description = "Email already exist")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @Valid @RequestBody UpdateUserDto updateUserDto) {
         if (!userService.isExists(id)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -62,6 +87,11 @@ public class UserController {
     }
 
     @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Delete a single user passing the user id")
+    @ApiResponse(responseCode = "204", description = "User deleted successfully")
+    @ApiResponse(responseCode = "403", description = "Forbidden request")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         if (!userService.isExists(id)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
