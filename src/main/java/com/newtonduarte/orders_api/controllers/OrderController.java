8a981db73a1f5600ed9a5b8d9174407d5dc4dd6e
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-24 10:23:57 -0300

    chore: add documentation to each order api endpoint

--- src/main/java/com/newtonduarte/orders_api/controllers/OrderController.java
+++ src/main/java/com/newtonduarte/orders_api/controllers/OrderController.java
@@ -1,7 +1,7 @@
 package com.newtonduarte.orders_api.controllers;
 
 import com.newtonduarte.orders_api.config.SecurityConfig;
-import com.newtonduarte.orders_api.domain.CreateOrderRequest;
+import com.newtonduarte.orders_api.domain.dto.ApiErrorResponse;
 import com.newtonduarte.orders_api.domain.dto.CreateOrderDto;
 import com.newtonduarte.orders_api.domain.dto.OrderDto;
 import com.newtonduarte.orders_api.domain.dto.UpdateOrderDto;
@@ -9,7 +9,10 @@ import com.newtonduarte.orders_api.domain.entities.OrderEntity;
 import com.newtonduarte.orders_api.mappers.OrderMapper;
 import com.newtonduarte.orders_api.services.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -31,9 +34,15 @@ public class OrderController {
 
     @GetMapping
     @Operation(summary = "Get a list of orders")
-    @ApiResponse(responseCode = "200", description = "Request success")
-    @ApiResponse(responseCode = "403", description = "Forbidden request")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Request success"),
+            @ApiResponse(responseCode = "403", description = "Forbidden request (Requires auth)", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            })
+    })
     public ResponseEntity<List<OrderDto>> getOrders() {
         List<OrderEntity> orders = orderService.getOrders();
         List<OrderDto> ordersDto = orders.stream().map(orderMapper::toDto).toList();
@@ -42,10 +51,18 @@ public class OrderController {
 
     @GetMapping(path = "/{id}")
     @Operation(summary = "Get a single order passing by ID")
-    @ApiResponse(responseCode = "200", description = "Request success")
-    @ApiResponse(responseCode = "403", description = "Forbidden request")
-    @ApiResponse(responseCode = "404", description = "Order not found")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Request success"),
+            @ApiResponse(responseCode = "403", description = "Forbidden request (Requires auth)", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Order not found", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            })
+    })
     public ResponseEntity<OrderDto> getOrder(@PathVariable Long id) {
         OrderEntity orderEntity = orderService.getOrder(id);
         return ResponseEntity.ok(orderMapper.toDto(orderEntity));
@@ -53,10 +70,18 @@ public class OrderController {
 
     @PostMapping
     @Operation(summary = "Create a single order passing the required fields")
-    @ApiResponse(responseCode = "201", description = "Order created successfully")
-    @ApiResponse(responseCode = "400", description = "Invalid request body")
-    @ApiResponse(responseCode = "403", description = "Forbidden request")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Order created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "403", description = "Forbidden request (Requires auth)", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            })
+    })
     public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody CreateOrderDto createOrderDto) {
         OrderEntity orderEntity = orderService.createOrder(orderMapper.toCreateOrderRequest(createOrderDto));
         return new ResponseEntity<>(orderMapper.toDto(orderEntity), HttpStatus.CREATED);
@@ -64,11 +89,21 @@ public class OrderController {
 
     @PutMapping(path = "/{id}")
     @Operation(summary = "Update a single order passing the order id and the required fields")
-    @ApiResponse(responseCode = "200", description = "Order updated successfully")
-    @ApiResponse(responseCode = "400", description = "Invalid request body")
-    @ApiResponse(responseCode = "403", description = "Forbidden request")
-    @ApiResponse(responseCode = "404", description = "Order not found")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Order updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "403", description = "Forbidden request (Requires auth)", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Order not found", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            })
+    })
     public ResponseEntity<OrderDto> updateOrder(@PathVariable Long id, @Valid @RequestBody UpdateOrderDto updateOrderDto) {
         OrderEntity updateOrder = orderService.updateOrder(id, orderMapper.toUpdateOrderRequest(updateOrderDto));
         return ResponseEntity.ok(orderMapper.toDto(updateOrder));
@@ -76,10 +111,20 @@ public class OrderController {
 
     @DeleteMapping(path = "/{id}")
     @Operation(summary = "Delete a single order passing the order id")
-    @ApiResponse(responseCode = "204", description = "Order deleted successfully")
-    @ApiResponse(responseCode = "403", description = "Forbidden request")
-    @ApiResponse(responseCode = "404", description = "Order not found")
-    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Order deleted successfully", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            }),
+            @ApiResponse(responseCode = "403", description = "Forbidden request (Requires auth)", content = {
+                    @Content(schema = @Schema(implementation = Void.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Order not found", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {
+                    @Content(schema = @Schema(implementation = ApiErrorResponse.class))
+            })
+    })
     public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
         orderService.deleteOrder(id);
         return ResponseEntity.noContent().build();
