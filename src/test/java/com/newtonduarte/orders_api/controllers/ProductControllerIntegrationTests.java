6ef8510475a2741a2c993613f9171921049042a9
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-24 20:29:55 -0300

    test: add with mock user annotation on products controller integration tests

--- src/test/java/com/newtonduarte/orders_api/controllers/ProductControllerIntegrationTests.java
+++ src/test/java/com/newtonduarte/orders_api/controllers/ProductControllerIntegrationTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
+@WithMockUser(username = "mockuser@email.com", password = "mockuser", roles = {"USER"})
 public class ProductControllerIntegrationTests {
     private final MockMvc mockMvc;
     private final ProductService productService;
