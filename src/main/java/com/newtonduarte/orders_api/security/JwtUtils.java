6973f7f5a62aadbeca487d4460eaf48b1fd7f0fa
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-24 18:02:53 -0300

    chore: remove unnecessary method from jwt utils

--- src/main/java/com/newtonduarte/orders_api/security/JwtUtils.java
+++ src/main/java/com/newtonduarte/orders_api/security/JwtUtils.java
@@ -1,14 +1,5 @@
 package com.newtonduarte.orders_api.security;
 
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 public class JwtUtils {
     public final static long defaultExpiresIn = 86400;
-
-    private static String encodePassword(String password) {
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-        return passwordEncoder.encode(password);
-    }
 }
